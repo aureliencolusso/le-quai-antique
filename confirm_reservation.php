e747215c92d6411aaf1fd210792d123be946ee60
Aurélien Colusso <104270398+aureliencolusso@users.noreply.github.com>
2023-08-22 00:28:34 +0200

    Improved security
    Added "htmlspecialchars" functionality to prevent XSS attacks

--- confirm_reservation.php
+++ confirm_reservation.php
@@ -1,12 +1,11 @@
 <?php
-include('header.php');
 include('connexion_bdd.php');
+include('header.php');
 
-// Vérifie si les informations de réservation existent dans la session et ridirige vers reserver.php si ce n'est pas le cas
+// Vérifie si les informations de réservation existent dans la session, sinon redirige vers reserver.php
 if (isset($_SESSION['reservation_info'])) {
     $reservationInfo = $_SESSION['reservation_info'];
 } else {
-
     header("Location: reserver.php");
     exit();
 }
@@ -32,20 +31,20 @@ if (isset($_SESSION['reservation_info'])) {
         <div class="row justify-content-center">
             <div class="col-md-6 text-center mb-5">
                 <div class="col-md-12 text-center">
-                    <p><strong><?php echo $_SESSION['reservation_info']['name'] ?></strong>, votre réservation a bien été enregistré !</p>
+                    <p><strong><?php echo htmlspecialchars($_SESSION['reservation_info']['name']); ?></strong>, votre réservation a bien été enregistrée !</p>
                 </div>
                 <div class="infos-user">
                     <ul class="list-unstyled">
-                        <strong>Nom :</strong> <?php echo $_SESSION['reservation_info']['name']; ?><br />
-                        <strong>E-mail :</strong> <?php echo $_SESSION['reservation_info']['email']; ?><br />
-                        <strong>Téléphone :</strong> <?php echo $_SESSION['reservation_info']['phone_number']; ?><br />
-                        <strong>Menu :</strong> <?php echo $_SESSION['reservation_info']['menu'] ?><br />
-                        <strong>Convives :</strong> <?php echo $_SESSION['reservation_info']['nb_guests']; ?><br />
-                        <strong>Date :</strong> <?php echo $_SESSION['reservation_info']['date'] ?><br />
-                        <strong>Heure :</strong> <?php echo $_SESSION['reservation_info']['time'] ?><br />
-                        <strong>Allergies :</strong> <?php echo $_SESSION['reservation_info']['allergies'] ?><br />
-                        <strong>Autre informations :</strong> <?php echo $_SESSION['reservation_info']['other_info'] ?><br />
-                        <strong>Numéro de réservation :</strong> <?php echo $_SESSION['reservation_info']['id']; ?>
+                        <strong>Nom :</strong> <?php echo htmlspecialchars($_SESSION['reservation_info']['name']); ?><br />
+                        <strong>E-mail :</strong> <?php echo htmlspecialchars($_SESSION['reservation_info']['email']); ?><br />
+                        <strong>Téléphone :</strong> <?php echo htmlspecialchars($_SESSION['reservation_info']['phone_number']); ?><br />
+                        <strong>Menu :</strong> <?php echo htmlspecialchars($_SESSION['reservation_info']['menu']); ?><br />
+                        <strong>Convives :</strong> <?php echo htmlspecialchars($_SESSION['reservation_info']['nb_guests']); ?><br />
+                        <strong>Date :</strong> <?php echo htmlspecialchars($_SESSION['reservation_info']['date']); ?><br />
+                        <strong>Heure :</strong> <?php echo htmlspecialchars($_SESSION['reservation_info']['time']); ?><br />
+                        <strong>Allergies :</strong> <?php echo htmlspecialchars($_SESSION['reservation_info']['allergies']); ?><br />
+                        <strong>Autres informations :</strong> <?php echo htmlspecialchars($_SESSION['reservation_info']['other_info']); ?><br />
+                        <strong>Numéro de réservation :</strong> <?php echo htmlspecialchars($_SESSION['reservation_info']['id']); ?>
                     </ul>
                 </div>
             </div>
@@ -57,7 +56,6 @@ if (isset($_SESSION['reservation_info'])) {
 <div class="reserv container">
     <a href="del_reservation.php"><button type="button" class="btn btn-outline-success btn-reserve">Supprimer</button></a>
 </div>
-
 
 <!--Bouton Accueil-->
 <div class="connect container">
