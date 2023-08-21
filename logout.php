7d76d08175feaab97a68971be97f039def00eae1
Aurélien Colusso <104270398+aureliencolusso@users.noreply.github.com>
2023-08-22 00:39:29 +0200

    Update logout.php
    Change comments

--- logout.php
+++ logout.php
@@ -1,12 +1,12 @@
 <?php
-session_start(); // Démarrer la session
+session_start(); // Démarre la session
 
-// Détruire toutes les variables de session
+// Détruit toutes les variables de session
 $_SESSION = array();
 
 // Destruction de la session
 session_destroy();
 
-// Redirection vers la page de connexion
+// Redirige vers la page de connexion
 header("Location: login.php");
 exit();
