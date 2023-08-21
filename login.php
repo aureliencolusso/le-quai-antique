54f9ffa7c648e5ff8e8be32e39ee87655c187193
Aurélien Colusso <104270398+aureliencolusso@users.noreply.github.com>
2023-08-22 00:38:51 +0200

    Improved security
    Added comments, and prepared queries to prevent SQL injections

--- login.php
+++ login.php
@@ -2,101 +2,99 @@
 include('connexion_bdd.php');
 include('header.php');
 
+$error_message = "";
 
-// Vérification que le formulaire a été soumis
+// Vérifie que le formulaire a été soumis
 if (isset($_POST["btn-connect"])) {
-	// Récupération des valeurs du formulaire
-	$email = $_POST["email"];
-	$password = $_POST["password"];
+    // Récupère les valeurs du formulaire
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-	// Requête SQL qui récupère les informations de l'user
-	$sql = "SELECT * FROM users WHERE email = '$email'";
+    // Récupère les informations de l'utilisateur
+    $query = "SELECT * FROM users WHERE email = :email";
 
-	// Exécution de la requête
-	$resultat = mysqli_query($connexion, $sql);
+    // Prépare la requête
+    $stmt = $connexion->prepare($query);
+    $stmt->bindParam(':email', $email);
 
-	// Vérification de la réussite de la requête
-	if (!$resultat) {
-		die("Erreur dans la requête : " . mysqli_error($connexion));
-	}
+    // Exécute la requête
+    $stmt->execute();
 
-	// Récupération des résultats de la requête
-	$users = mysqli_fetch_assoc($resultat);
+    // Récupère les résultats de la requête
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-	if (password_verify($password, $users["password"])) {
-		// Le mot de passe est correct, l'utilisateur est authentifié
-		// Vous pouvez maintenant rediriger l'utilisateur vers une page sécurisée
-		$_SESSION['user_info'] = array(
-			'email' => $users['email'],
-			'name' => $users['name'],
-			'phone_number' => $users['phone_number'],
-			'nb_guests' => $users['nb_guests'],
-			'allergies' => $users['allergies'],
-			'menu' => $users['menu'],
-			'date' => $users['date'],
-			'time' => $users['time'],
-			'other_info' => $users['other_info'],
-			'is_admin' => $users['is_admin'],
-			'id' => $users['id']
-		);
+    // Si le mot de passe est correct, l'utilisateur est authentifié
+    if ($user && password_verify($password, $user["password"])) {
+        $_SESSION['user_info'] = array(
+            'email' => $user['email'],
+            'name' => $user['name'],
+            'phone_number' => $user['phone_number'],
+            'nb_guests' => $user['nb_guests'],
+            'allergies' => $user['allergies'],
+            'menu' => $user['menu'],
+            'date' => $user['date'],
+            'time' => $user['time'],
+            'other_info' => $user['other_info'],
+            'is_admin' => $user['is_admin'],
+            'id' => $user['id']
+        );
 
-		header("Location: profil.php");
-	} else {
-		// Le mot de passe est incorrect, l'utilisateur est refusé
-		// Vous pouvez afficher un message d'erreur à l'utilisateur
-		$error_message = "Email ou mot de passe incorrect";
-	}
+        header("Location: profil.php");
+        exit();
+    } else {
+        // Sinon, affiche un message d'erreur
+        $error_message = "Email ou mot de passe incorrect";
+    }
 }
 ?>
 
 <!--Titres-->
 <section class="banner d-flex justify-content-center align-items-center pt-5">
-	<div class="container my-5 py-5">
-		<div class="row">
-			<div class="col-md-12 text-center">
-				<h1 class="card-page-title">
-					<span class="bodonimt">Se connecter</span>
-				</h1>
-				<div class="separator"></div>
-			</div>
-		</div>
-	</div>
+    <div class="container my-5 py-5">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1 class="card-page-title">
+                    <span class="bodonimt">Se connecter</span>
+                </h1>
+                <div class="separator"></div>
+            </div>
+        </div>
+    </div>
 </section>
 
 <!--Formulaire de connexion-->
 <section class="connect-form py-5">
-	<div class="container">
-		<form method="POST" action="login.php">
-			<div class="row">
-				<div class="input-group mb-3">
-					<span class="input-group-text"><i class="fas fa-at"></i></span>
-					<input type="email" class="form-control" placeholder="Votre adresse email" name="email" required />
-				</div>
-				<div class="input-group mb-3">
-					<span class="input-group-text"><i class="fas fa-eye"></i></span>
-					<input type="password" class="form-control" placeholder="Votre mot de passe" name="password" required />
-				</div>
-			</div>
+    <div class="container">
+        <form method="POST" action="login.php">
+            <div class="row">
+                <div class="input-group mb-3">
+                    <span class="input-group-text"><i class="fas fa-at"></i></span>
+                    <input type="email" class="form-control" placeholder="Votre adresse email" name="email" required />
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text"><i class="fas fa-eye"></i></span>
+                    <input type="password" class="form-control" placeholder="Votre mot de passe" name="password" required />
+                </div>
+            </div>
 
-			<!-- Affichage du message d'erreur -->
-			<?php if (isset($error_message)) : ?>
-				<div class="alert alert-danger" role="alert">
-					<?php echo $error_message; ?>
-				</div>
-			<?php endif; ?>
+            <!-- Affichage du message d'erreur -->
+            <?php if (!empty($error_message)) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $error_message; ?>
+                </div>
+            <?php endif; ?>
 
-			<!--Bouton connexion-->
-			<div class="connect container">
-				<button type="submit" class="btn btn-outline-success btn-connect" name="btn-connect">Se connecter</button>
-			</div>
-		</form>
-	</div>
+            <!--Bouton connexion-->
+            <div class="connect container">
+                <button type="submit" class="btn btn-outline-success btn-connect" name="btn-connect">Se connecter</button>
+            </div>
+        </form>
+    </div>
 </section>
-
 
 <!--Bouton Inscription-->
 <div class="register container">
-	<a href="register.php"><button type="button" class="btn btn-outline-success btn-register">S'inscrire</button></a>
+    <a href="register.php"><button type="button" class="btn btn-outline-success btn-register">S'inscrire</button></a>
 </div>
 
 <?php include('footer.php'); ?>
